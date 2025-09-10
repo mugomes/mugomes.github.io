9f3c7365ed0caf91bac22e03408c2cec4464f655
mestredainfo <murilogomes5792@gmail.com>
2025-09-10 17:10:22 -0300

    Ajustado design com bootstrap

--- js/script.js
+++ js/script.js
@@ -1,55 +1,38 @@
+// Atualiza o ano no rodapé
 document.getElementById('year').textContent = new Date().getFullYear();
-const toggle = document.getElementById('menuToggle');
-const navList = document.getElementById('navList');
-toggle.addEventListener('click', () => {
-    navList.classList.toggle('open');
-    const expanded = toggle.getAttribute('aria-expanded') === 'true';
-    toggle.setAttribute('aria-expanded', !expanded);
-});
-navList.querySelectorAll('a').forEach(a => a.addEventListener('click', () => {
-    navList.classList.remove('open');
-    toggle.setAttribute('aria-expanded', 'false');
-}));
 
-const channelId = "UCWtiMUKu0pE9UFlDLy7nN2A"; // coloque seu channel_id aqui
+// Galeria de Vídeos
+const channelId = "UCWtiMUKu0pE9UFlDLy7nN2A"; // seu channel_id
 const url = `https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
-
 const gallery = document.getElementById("gallery");
-const modal = document.getElementById("videoModal");
+const modal = new bootstrap.Modal(document.getElementById("videoModal"));
 const videoFrame = document.getElementById("videoFrame");
-const closeModal = document.getElementById("closeModal");
 
 fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        const videos = data.items.slice(0, 6); // pega só os 6 primeiros
-
-        videos.forEach(video => {
-            const videoId = video.link.split("v=")[1];
-            const div = document.createElement("div");
-            div.classList.add("video");
-            div.innerHTML = `
-            <img src="https://img.youtube.com/vi/${videoId}/hqdefault.jpg" alt="${video.title}">
-            <h3>${video.title}</h3>
+  .then(res => res.json())
+  .then(data => {
+    const videos = data.items.slice(0, 6);
+    videos.forEach(video => {
+      const videoId = video.link.split("v=")[1];
+      const col = document.createElement("div");
+      col.classList.add("col-md-4");
+      col.innerHTML = `
+            <div class="card video shadow-sm h-100 cursor-pointer">
+              <img src="https://img.youtube.com/vi/${videoId}/hqdefault.jpg" class="card-img-top" alt="${video.title}">
+              <div class="card-body">
+                <h5 class="card-title">${video.title}</h5>
+              </div>
+            </div>
           `;
-            div.addEventListener("click", () => {
-                videoFrame.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-                modal.style.display = "flex";
-            });
-            gallery.appendChild(div);
-        });
+      col.querySelector(".video").addEventListener("click", () => {
+        videoFrame.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        modal.show();
+      });
+      gallery.appendChild(col);
     });
+  });
 
-// Fechar modal
-closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
-    videoFrame.src = "";
-});
-
-// Fechar clicando fora do vídeo
-modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        modal.style.display = "none";
-        videoFrame.src = "";
-    }
+// Limpa src ao fechar modal
+document.getElementById("videoModal").addEventListener("hidden.bs.modal", () => {
+  videoFrame.src = "";
 });
